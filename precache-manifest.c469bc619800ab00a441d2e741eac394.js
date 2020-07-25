58e201a23f843efd4cbf01a21f78b50b2d190c7b
Charith Jayasanka <charithjayasanka2@gmail.com>
2020-07-26 00:30:24 +0530

    Updates

---
+++ precache-manifest.c469bc619800ab00a441d2e741eac394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb60ba5605444eb0afbb062ee2ce921",
+    "url": "/test-gh-pages/index.html"
+  },
+  {
+    "revision": "7f8ebbd32b9865fc58ba",
+    "url": "/test-gh-pages/static/css/main.33cff14a.chunk.css"
+  },
+  {
+    "revision": "c2ff1fd511158cc19b19",
+    "url": "/test-gh-pages/static/js/2.65ecc9c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-gh-pages/static/js/2.65ecc9c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8ebbd32b9865fc58ba",
+    "url": "/test-gh-pages/static/js/main.e489c262.chunk.js"
+  },
+  {
+    "revision": "22da01948750efc4e02a",
+    "url": "/test-gh-pages/static/js/runtime-main.f0920a6a.js"
+  }
+]);
